d30b5776b55d9bf55a52893a916fdb4027374a1a
shkilya <shkilya1994@gmail.com>
2018-01-28 10:46:15 +0200

    generate crud for BOOK

---
+++ frontend/views/book/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\BookSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Books';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="book-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Book', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'date_create',
+            'date_update',
+            'date',
+            //'preview',
+            //'author_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>
+</div>

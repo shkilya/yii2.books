5888f85f73d24b4ea5289f2f34b74f1150d344b8
shkilya <shkilya1994@gmail.com>
2018-01-28 10:05:55 +0200

    create  model :  Author

---
+++ frontend/models/Authors.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "authors".
+ *
+ * @property int $id
+ * @property string $firstname
+ * @property string $lastname
+ */
+class Authors extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'authors';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['firstname', 'lastname'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
+        ];
+    }
+}
